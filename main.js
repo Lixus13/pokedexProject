ae160e36dc4dd7766eeb60ae89241a6f9709d794
lixus13 <c.luis013@gmail.com>
2019-11-21 01:31:59 -0500

    playing with incorporating methods

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
 let senpaiArray = []
 
+
 queryPokemonAPI =  async () => {
-  let x = prompt('Enter PokeName or PokeNumber')
-  let req =  await fetch(`https://pokeapi.co/api/v2/pokemon/${x}/`)
-  let data =  await req.json()
-  let pokemon = {
-    name: data.name,
-    link: data.sprites.front_default,
-    stats: {
+  var x = prompt('Enter PokeName or PokeNumber'),
+   req =  await fetch(`https://pokeapi.co/api/v2/pokemon/${x}/`),
+   data =  await req.json(),
+   pokemon = {
+     name: data.name,
+     link: data.sprites.front_default,
+     stats: {
       hp: data.stats[5].base_stat,
       atk: data.stats[4].base_stat,
       def: data.stats[3].base_stat
-    },
-    abl: [data.abilities[0].ability.name]
+      },
+     ablt: function {
+     for (let i = 0; i <= array.length; i++) {
+
+      }
+     }
   }
-  console.log(data);
-  senpaiArray.push(pokemon)
-  createPkmn(pokemon)
+  console.log(data)
+  document.getElementById("stats").innerHTML = txt;
+  senpaiArray.push(pokemon.name)
+
+
 }
 
-createPkmn = (pokemon) => {
-  let generate = document.createElement('ul')
-  document.body.appendChild(generate)
-  generate.setAttribute("id", "generator_id");
-  generate.innerHTML= `<img src = "${pokemon.link}" width="150px" height="150px">
-                        <li> ${pokemon.name}</li>
-                        <li> Pokemon HP: ${pokemon.stats.hp}</li>
-                        <li>Pokemon DEF: ${pokemon.stats.def}</li>
-                        <li>Pokemon ATK: ${pokemon.stats.atk}</li>
-                        <li>Pokemon Abilities: ${pokemon.abl[0]} </li>
-                      `
-}
+// createPkmn = (pokemon) => {
+//   let generate = document.createElement('ul')
+//   generate.setAttribute("id", "generator_id");
+//   generate.innerHTML= `<img src = "${pokemon.link}" width="150px" height="150px">
+//                         <li> ${pokemon.name}</li>
+//                         <li> Pokemon HP: ${pokemon.stats.hp}</li>
+//                         <li>Pokemon DEF: ${pokemon.stats.def}</li>
+//                         <li>Pokemon ATK: ${pokemon.stats.atk}</li>
+//                         <li>Pokemon Abilities: ${pokemon.ablt}</li>
+//                       `
+//   document.body.appendChild(generate)
+// }
