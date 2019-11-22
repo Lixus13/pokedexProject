85fec0644511bb0d9b69f7e754ad1d2a4e72793d
Luis Carmona <lcarmona@nyc.yearup.org>
2019-11-22 11:25:26 -0500

    added comments

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ queryPokemonAPI =  async () => {
   let pokemon = new Pokemon()
     pokemon.name = data.name
     pokemon.id = data.id
+    // pokemon.sprite =  data.sprites.front_default
     pokemon.sprite = `https://www.pkparaiso.com/imagenes/xy/sprites/animados/${x}.gif`
     pokemon.hp =  data.stats[5].base_stat
     pokemon.atk = data.stats[4].base_stat
@@ -28,21 +29,25 @@ createPkmn = (pokemon) => {
     stats.style.display = 'block'
   }
 
+
   let generate = document.createElement('ul')
   let pokemengs = document.getElementById('digimon')
-  generate.setAttribute("id", "generator");
-  generate.setAttribute("onclick", "statistics()");
+  generate.setAttribute("id", "generator")
+  generate.setAttribute("onclick", "statistics()")
+
   generate.innerHTML= `<img src = "${pokemon.sprite}">
                         <li> ${pokemon.id}. ${pokemon.name}</li>
                         `
-  stats.innerHTML = `   <h2><ul><b> PokéStats </b></ul></h2>
+
+  stats.innerHTML =     `
+                        <h2><ul><b> PokéStats </b></ul></h2>
                         <li> Pokemon HP: ${pokemon.hp}</li>
                         <li>Pokemon ATK: ${pokemon.atk}</li>
                         <li>Pokemon DEF: ${pokemon.def}</li>
                         <li>Pokemon Abilities: ${pokemon.abilities} </li>
-                        <li>Price: $3,000,000.00</li>
+                        <li>Price: ¥3,000,000.00</li>
                           `
 
                       pokemengs.appendChild(generate)
-
+// var(math.random()-constant)*scale
 }
